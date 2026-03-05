9e01885703d962a2f2d7b865b5e0da054064f623
Achim Thesmann <achim@google.com>
2026-03-05 10:42:48 -0800

    Revert^2: Do not enforce DCL restrictions before C
    While the change is already target SDK gated, this additinally protects an app that targets C but runs on a B build from the current source code.
    
    This reverts ag/38370507.
    
    The revert is clean, except that the version code constant has been renamed in the meantime (C -> CINNAMON_BUN).
    
    Bug: 483473472
    Bug: 463348571
    Test: Manual
    Flag: EXEMPT BUG_FIX
    Change-Id: Icd15719fa80191bbbdc0e02d86193713efa06b94

--- ojluni/src/main/java/java/lang/Runtime.java
+++ ojluni/src/main/java/java/lang/Runtime.java
@@ -949,11 +949,9 @@ public class Runtime {
                         fromClass.getPackageName(),
                         file.getName());
             }
-            // TODO: before the read_only_dynamic_code_load_throw_exception flag is pushed to
-            // NextFood, we will need to add the if (VMRuntime.getSdkVersion() >= VersionCodes.C)
-            // check here.
             if (VMRuntime.isReadOnlyDynamicCodeLoadThrowExceptionEnabled()
-                    && VMRuntime.isThrowErrorForWritableDclEnabled()) {
+                    && VMRuntime.isThrowErrorForWritableDclEnabled()
+                    && VMRuntime.getSdkVersion() >= VersionCodes.CINNAMON_BUN) {
                 throw new UnsatisfiedLinkError(
                   "Attempt to load writable file: " + filename);
             }
